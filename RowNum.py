3da28ca1eac26332763caece7c7f579452c23607
aaroncarbonell <44853998+aaroncarbonell@users.noreply.github.com>
2023-10-24 13:12:25 -0400

    Add files via upload

---
+++ RowNum.py
@@ -0,0 +1,31 @@
+#Aaron Carbonell
+#INF103
+#pg215 #4 #Roman Numerals #Coach K vers.
+
+num = int(input('Enter a single number from 1-10:'))
+
+if num == 1:
+    print('I')
+elif num == 2:
+    print('II')
+elif num == 3:
+    print('III')
+elif num == 4:
+    print('IV')
+elif num == 5:
+    print('V')
+elif num == 6:
+    print('VI')
+elif num == 7:
+    print('VII')
+elif num == 8:
+    print('VIII')
+elif num == 9:
+    print('IX')
+elif numb == 10:
+    print('X')
+else:
+    print('Error: Number out of range')
+            
+    
+
---
+++ Tips, Tax, Total.py
@@ -0,0 +1,7 @@
+#Aaron Carbonell
+#INF103
+# Tips, Tax, and Total (Ex. 8 p. 160)
+
+#Write a program that calculates the total amount of a meal purchased at a restaurant. 
+#The program should ask the user to enter the charge for the food, then calculate the amounts of
+#percent tip and 7 percent sales tax. Display each of these amounts and the total.
